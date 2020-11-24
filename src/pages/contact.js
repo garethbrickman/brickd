2c371be0873e1ab59aa28f949125714f4214ec07
Gareth Brickman <brickstick@gmail.com>
2020-11-24 00:12:50 -0500

    [DEV] Add aria labels to social icons"

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,11 +6,11 @@ import SEO from "../components/seo"
 
 const ContactPage = ({ data }) => (
   <Layout>
-    <SEO title="Resume" />
-    <h1><a href="https://github.com/garethbrickman/" target="_blank" rel="noopener noreferrer"><SiGithub /></a></h1>
-    <h1><a href="https://www.linkedin.com/in/br1ck/" target="_blank" rel="noopener noreferrer"><SiLinkedin /></a></h1>
-    <h1><a href="https://twitter.com/GarethBrickman" target="_blank" rel="noopener noreferrer"><SiTwitter /></a></h1>
-    <h1><a href="mailto:gbrickman@pm.me"><SiMailDotRu /></a></h1>
+    <SEO title="Contact" />
+    <h1><a href="https://github.com/garethbrickman/" target="_blank" rel="noopener noreferrer"><SiGithub aria-label="Github link"/></a></h1>
+    <h1><a href="https://www.linkedin.com/in/br1ck/" target="_blank" rel="noopener noreferrer"><SiLinkedin aria-label="LinkedIn link"/></a></h1>
+    <h1><a href="https://twitter.com/GarethBrickman" target="_blank" rel="noopener noreferrer"><SiTwitter aria-label="Twitter link"/></a></h1>
+    <h1><a href="mailto:gbrickman@pm.me"><SiMailDotRu aria-label="Email address"/></a></h1>
 
   </Layout>
 )
