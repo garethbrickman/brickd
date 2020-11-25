041000985917ccbc5f2c9a3c8dca6058c063b03c
Gareth Brickman <brickstick@gmail.com>
2020-11-25 09:35:01 -0500

    [FIX] Typo

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ const AboutPage = ({ data }) => (
     <p>I’m Brick, a full stack software developer and founder of <a href="https://hinative.com/en-US/questions/4746009#answer-12677944"
     target="_blank" rel="noopener noreferrer">Suave Na Nave LLC</a>.</p>
     <p>My two prime motivators are deep desires to learn and rapidly figure unfamiliar tech out and to be practically and meaningfully helpful.</p>
-    <p>In my tech experience doing web deployments and backend ops work I have found I most enjoyed playing the tech detective. I love sleuthing out complex problems on-the-fly; from obscure compiler error messages to reconciling tech stack integration quirks to pinpointing that fat finger human error within the codebase.</p>
+    <p>In my tech experience doing web deployments and backend ops work I have found I most enjoyed playing the tech detective. I love sleuthing out complex problems on the fly; from obscure compiler error messages to reconciling tech stack integration quirks to pinpointing that fat finger human error within the codebase.</p>
     <p><a href="https://www.holbertonschool.com"
     target="_blank" rel="noopener noreferrer">Holberton School’s</a> expansive full stack curriculum exposed us to multiple tech stacks where learning fast and pinpointing issues are key skills to have. </p>
     <br/>
