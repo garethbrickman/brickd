eede642bf7e04f801b3ad43c7dcfb5b3f7473698
Gareth Brickman <brickstick@gmail.com>
2020-12-14 18:12:14 -0500

    Add my name to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer container">
     <div className="footer__item footer__copyright">
-      © {new Date().getFullYear()} Suave Na Nave LLC. Built with 
+      © {new Date().getFullYear()} Gareth Brickman / Suave Na Nave LLC. Built with 
       {` `}
       <a href="https://www.gatsbyjs.org"
       target="_blank" rel="noopener noreferrer">Gatsby</a>.
