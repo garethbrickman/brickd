6fc232c83219da2b0fd8e5f67ff03ed3e27ceb76
Gareth Brickman <brickstick@gmail.com>
2021-02-26 14:37:34 -0500

    Rework page content and structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,28 +8,29 @@ import Img from 'gatsby-image';
 const AboutPage = ({ data }) => (
   <Layout>
     <SEO title="About" />
-    <p>I’m Brick, a full stack software developer and founder of <a href="https://hinative.com/en-US/questions/4746009#answer-12677944"
-    target="_blank" rel="noopener noreferrer">Suave Na Nave LLC</a>.</p>
-    <p>My two prime motivators are deep desires to learn and rapidly figure unfamiliar tech out and to be practically and meaningfully helpful.</p>
-    <p>In my tech experience doing web deployments and backend ops work I have found I most enjoyed playing the tech detective. I love sleuthing out complex problems on the fly; from obscure compiler error messages to reconciling tech stack integration quirks to pinpointing that fat finger human error within the codebase.</p>
-    <p><a href="https://www.holbertonschool.com"
-    target="_blank" rel="noopener noreferrer">Holberton School’s</a> expansive full stack web developer curriculum exposed us to multiple tech stacks where learning fast and pinpointing issues are key skills to have. </p>
+    <p>I’m Brick, a computer figure outerer and founder of <em>Suave Na Nave LLC</em>. That's a Brazilian Portuguese saying that means "supercool", which I aspire to always be.</p>
+    <p>I’ve been tinkering with computers since Netscape, 56k modems and IRC were bleeding edge.</p>
+    <p>I have now taken my decades of tinkering pro and graduated from an intensive 18-month higher education program that trained me in software engineering and full stack web development.</p>
+    <p>I believe software development is a craft to be honed and requires mastering arts and sciences alike. My two prime motivators are deep desires to learn and rapidly figure new and old tech out and to be practically and meaningfully helpful with these skills.</p>
+    <p>My software tech experience:</p>
+    <ul>
+    <li><b>Frontend:</b> HTML, CSS, Javascript (ES6 and a little Typescript), ReactJS, GatsbyJS, Sass, Bootstrap, TailwindCSS.</li>
+    <p></p>
+    <li><b>Backend:</b> C (comp sci fundamentals, data structures & algorithms), RestAPIs (Python with Flask and Javascript with ExpressJS), Databases (MySQL predominantly, a little Redis, a little GraphQL and a little MongoDB for NoSQL).</li>
+    <p></p>
+    <li><b>Sysadmin:</b> Git (all our projects were built, uploaded and reviewed on Github), Unix shell scripts (Bash, zsh), remote server management (Ubuntu Server/Debian, a little bit of Docker), VM management (VirtualBox, vagrant), in-house web server management (Nginx and Apache), cloud services and deployments (AWS S3, AWS Amplify, a little AWS EC2, Digital Ocean droplets). I have also done a little DNS configuration with Google Domains and AWS Route 53.</li>
+    <p></p>
+    <li><b>Other Skills:</b> Project-based problem solving, whiteboarding ideas and solutions, collaboration and teamwork with multiple onsite projects in pairs and larger teams, remote collaboration with asynchronous communication, project management using Kanban boards and Agile-ish methodology, public speaking and presentations.</li>
+    </ul>
     <br/>
     <hr/>
-    <p>The construction of this site would not have been possible without troubleshooting from:</p>
+    <p>The construction of this site would not have been possible without troubleshooting the public work of:</p>
     <p><a href="https://jenniferwadella.com/blog/all-the-dumb-mistakes-i-made-building-my-first-gatsby-site"
-    target="_blank" rel="noopener noreferrer">Jennifer Wadella</a></p>
-    <p><a href="https://chenhuijing.com/blog/flexbox-and-absolute-positioning/#%F0%9F%91%9F"
-    target="_blank" rel="noopener noreferrer">Chen Hui Jing</a></p>
-    <p><a href="https://github.com/gaearon/overreacted.io"
-    target="_blank" rel="noopener noreferrer">Dan Abramov</a></p>
-        <p><a href="https://medium.com/@kyle.robert.gill/ridiculously-easy-image-optimization-with-gatsby-js-59d48e15db6e"
-    target="_blank" rel="noopener noreferrer">Kyle Gill</a></p>
-    <p><a href="https://www.orangejellyfish.com/blog/a-comprehensive-guide-to-images-in-gatsby/"
-    target="_blank" rel="noopener noreferrer">James Allardice</a></p>
-    <p>And most of all</p>
-    <p><a href="https://dennytek.com/"
-    target="_blank" rel="noopener noreferrer">Denny Tek</a></p>
+    target="_blank" rel="noopener noreferrer">Jennifer Wadella</a>, <a href="https://chenhuijing.com/blog/flexbox-and-absolute-positioning/#%F0%9F%91%9F"
+    target="_blank" rel="noopener noreferrer">Chen Hui Jing</a>, <a href="https://github.com/gaearon/overreacted.io"
+    target="_blank" rel="noopener noreferrer">Dan Abramov</a>, <a href="https://medium.com/@kyle.robert.gill/ridiculously-easy-image-optimization-with-gatsby-js-59d48e15db6e"
+    target="_blank" rel="noopener noreferrer">Kyle Gill</a>, <a href="https://www.orangejellyfish.com/blog/a-comprehensive-guide-to-images-in-gatsby/"
+    target="_blank" rel="noopener noreferrer">James Allardice</a>. And most of all: <a href="https://dennytek.com/" target="_blank" rel="noopener noreferrer">Denny Tek</a>.</p>
     <p>Thank you all.</p>
     <div class="about-img" style={{  maxWidth: `400px`, marginBottom: `1.45rem` }}>
     <Img fluid={data.file.childImageSharp.fluid} alt="brick again" title="brick again" />
